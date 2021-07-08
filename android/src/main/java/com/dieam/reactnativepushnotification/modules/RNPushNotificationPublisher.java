fdf3b735df4715463bc8b233902cca3a7d6f737c
Sourabh Kumar <sourabh.ku@vedantu.com>
2021-07-08 14:40:16 +0530

    When Local Notification fire: DISPLAY_NOTIFICATION Event fire with Notification Payload

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationPublisher.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationPublisher.java
@@ -12,6 +12,14 @@ import java.security.SecureRandom;
 
 import static com.dieam.reactnativepushnotification.modules.RNPushNotification.LOG_TAG;
 
+import java.util.Iterator;
+import java.util.Set;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.ReactContext;
+
 public class RNPushNotificationPublisher extends BroadcastReceiver {
     final static String NOTIFICATION_ID = "notificationId";
 
@@ -29,6 +37,18 @@ public class RNPushNotificationPublisher extends BroadcastReceiver {
         handleLocalNotification(context, bundle);
     }
 
+    public static WritableMap bundleToMap(Bundle extras) {
+        WritableMap map = Arguments.createMap();
+
+        Set<String> ks = extras.keySet();
+        Iterator<String> iterator = ks.iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            map.putString(key, extras.getString(key));
+        }/*from   w ww .j  a  v  a 2s .c  o m*/
+        return map;
+    }
+
     private void handleLocalNotification(Context context, Bundle bundle) {
 
         // If notification ID is not provided by the user for push notification, generate one at random
@@ -41,6 +61,14 @@ public class RNPushNotificationPublisher extends BroadcastReceiver {
         RNPushNotificationHelper pushNotificationHelper = new RNPushNotificationHelper(applicationContext);
         
         Log.v(LOG_TAG, "sendNotification: " + bundle);
+
+        // NEW CODE: Construct and load our normal React JS code bundle
+        final ReactInstanceManager mReactInstanceManager = ((ReactApplication) applicationContext).getReactNativeHost().getReactInstanceManager();
+        ReactContext RCcontext = mReactInstanceManager.getCurrentReactContext();
+
+        RNPushNotificationJsDelivery jsDelivery = new RNPushNotificationJsDelivery(RCcontext);
+        WritableMap params = bundleToMap(bundle);
+        jsDelivery.sendEvent("DISPLAY_NOTIFICATION", params);
 
         pushNotificationHelper.sendToNotificationCentre(bundle);
     }
